d4b9ae87cc2b208789157a778ca42d58b1f17b0d
nerd35 <umukorochinedu@gmail.com>
2020-06-08 15:19:06 +0100

    added checkout item and also integrated the cartItem in to the checkout page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import ShopPage from './pages/shop/Shop.component';
 import Header from './components/header/Header.component';
 import './App.css';
 import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -44,6 +47,7 @@ class App extends React.Component {
 				<Switch>
 					<Route exact path='/' component={HomePage} />
 					<Route  path='/shop' component={ShopPage} />
+					<Route exact  path='/checkout' component={CheckoutPage} />
 					<Route exact path='/signin' render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUp />)} />
 				</Switch>
 			</div>
